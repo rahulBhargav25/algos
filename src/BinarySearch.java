1ac09dc5d7fa573387a12751c2d34c272cccab4e
Rahul Bhargav <rahulbhargav26@gmail.com>
2021-09-27 15:06:41 +0530

    Recursion Binary Search

--- src/BinarySearch.java
+++ src/BinarySearch.java
@@ -1,12 +1,24 @@
+import java.util.Arrays;
 
 public class BinarySearch {
-
+	static int low = 0;
 	public static void main(String[] args) {
-		int n = 5;
-		int low = 0;
-		int high = n - 1;
+		
+		
+		
 		int[] arr = {2,3,4,5,6};
+		int n = arr.length;
+		int high = n - 1;
 		int item = 6;
+		
+		//Binary search using recursion
+		int ans = recurrBinarySearch(arr, high, item);
+		System.out.println(ans + " is found at index "+findIndex(arr, ans));
+		
+		
+		
+		
+		
 		while(low<=high) {
 			int mid = (low+high)/2;
 			int guess = arr[mid];
@@ -25,6 +37,30 @@ public class BinarySearch {
 			}
 		}
 
+	}
+	
+    public static int findIndex(int arr[], int t)
+    {
+ 
+        int index = Arrays.binarySearch(arr, t);
+        return (index < 0) ? -1 : index;
+    }
+	
+	
+	public static int recurrBinarySearch(int[] arr, int high, int item) {
+		int mid = (low+high)/2;
+		int guess =arr[mid];
+		if(guess==item) {
+			return guess;
+		} else if(guess>item) {
+			high = mid-1;
+			return recurrBinarySearch(arr, high, item);
+		} else if(guess<item) {
+			low = mid+1;
+			return recurrBinarySearch(arr, high, item);
+		} else {
+			return 0;
+		}
 	}
 
 }
