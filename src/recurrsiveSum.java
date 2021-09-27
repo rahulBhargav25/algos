614d7ac8988f5b91b3b97700022830c7f8b476a4
Rahul Bhargav <rahulbhargav26@gmail.com>
2021-09-27 14:41:32 +0530

    find biggest number in an array using recurssion

--- src/recurrsiveSum.java
+++ src/recurrsiveSum.java
@@ -1,12 +1,14 @@
 
 public class recurrsiveSum {
 	static int i = 0;
+	static int j=-1;
 	static int x;
 	public static void main(String[] args) {
-		int[] arr= {2,4,6,7};
+		int[] arr= {7,6,50,4,12,25,35,40,5,11};
 		int numOfElements = number(arr) -1;
 		System.out.println(sum(arr, arr.length));
 		System.out.println(numOfElements);
+		System.out.println(biggestNumber(arr));
 
 	}
 	
@@ -25,6 +27,24 @@ public class recurrsiveSum {
 			x++;
 			number(arr);
 			return x;
+		} else {
+			return 0;
+		}
+	}
+	
+	public static int biggestNumber(int[] arr) {
+		j++;
+		if(j!=arr.length-1 && arr[j]>arr[j+1] ) {
+			int temp = arr[j+1];
+			arr[j+1] = arr[j];
+			arr[j] = temp;
+			
+			biggestNumber(arr);
+			return arr[j];
+			
+		} else if (j!=arr.length-1 && arr[j]<arr[j+1]){
+			biggestNumber(arr);
+			return arr[j];
 		} else {
 			return 0;
 		}
