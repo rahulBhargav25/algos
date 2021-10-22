8d7a1edc54ff55883909b3c683e93b3dffe8b57d
Rahul Bhargav <rahulbhargav26@gmail.com>
2021-10-22 08:59:56 +0530

    find major element in an array nlon approach without for loop

--- src/majorityelement.java
+++ src/majorityelement.java
@@ -2,10 +2,10 @@ import java.util.Arrays;
 
 public class majorityelement {
 	public static void main(String[] args) {
-		int[] arr = {1,2,3,1,2,1,1};
+		int[] arr = {1,2,3,1,2,2,2};
 		int n = arr.length;
 		int major = n/2;
-		Arrays.sort(arr);
+		Arrays.sort(arr); //--time complexity nlogn
 		//int majoritem = 0;
 		int c = 1;
 //		for(int i = 0; i<n;i++) {
@@ -24,16 +24,20 @@ public class majorityelement {
 //			}
 //		}
 		
-		for(int i = 1; i < n; i++) {
-			if(arr[i]==arr[i-1]) {
-				c++;
-			} else {
-				c=1;
-			} if(c>=major) {
-				System.out.println(arr[i]);
-				break;
-			}
-		}
+//		for(int i = 1; i < n; i++) {    //--time complexity n
+//			if(arr[i]==arr[i-1]) {
+//				c++;
+//			} else {
+//				c=1;
+//			} if(c>=major) {
+//				System.out.println(arr[i]);
+//				break;
+//			}
+//		}
+		
+		//simplest approach ---also nlogn but no need of for loop
+		
+		System.out.println(arr[n/2]);
 		
 	}
-}
+}  //--total time complexity nlogn
