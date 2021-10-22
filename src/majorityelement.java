e1487d062b89236a1b20f42a9dee40629360fa59
Rahul Bhargav <rahulbhargav26@gmail.com>
2021-10-22 10:32:09 +0530

    find major element in an array o(n) morre's voting algorithm

--- src/majorityelement.java
+++ src/majorityelement.java
@@ -2,42 +2,69 @@ import java.util.Arrays;
 
 public class majorityelement {
 	public static void main(String[] args) {
-		int[] arr = {1,2,3,1,2,2,2};
+		int[] arr = {1,2,3,1,1,1,2};
 		int n = arr.length;
-		int major = n/2;
-		Arrays.sort(arr); //--time complexity nlogn
+		
+		 //--time complexity nlogn
 		//int majoritem = 0;
 		int c = 1;
-//		for(int i = 0; i<n;i++) {
-//			
-//			for(int j = i+1; j < n; j++) {
-//				if(arr[i]>=arr[j]) {
-//					majoritem++;
-//					if(majoritem==major) {
-//						System.out.println(arr[i]);
-//					} else {
-//						continue;
-//					}
-//				} else {
-//					continue;
-//				}
-//			}
-//		}
-		
-//		for(int i = 1; i < n; i++) {    //--time complexity n
-//			if(arr[i]==arr[i-1]) {
-//				c++;
-//			} else {
-//				c=1;
-//			} if(c>=major) {
-//				System.out.println(arr[i]);
-//				break;
-//			}
-//		}
+		int dominantElement = -1; 
+
+//Bruteforce approch		
+/*		for(int i = 0; i<n;i++) {
+			
+			for(int j = i+1; j < n; j++) {
+				if(arr[i]>=arr[j]) {
+					majoritem++;
+					if(majoritem==major) {
+						System.out.println(arr[i]);
+					} else {
+						continue;
+					}
+				} else {
+					continue;
+				}
+			}
+		}*/
+		// one where array is sorted we can find the dominant element using for loop
+/*		for(int i = 1; i < n; i++) {    //--time complexity n
+			if(arr[i]==arr[i-1]) {
+				c++;
+			} else {
+				c=1;
+			} if(c>=major) {
+				System.out.println(arr[i]);
+				break;
+			}
+		}*/
 		
 		//simplest approach ---also nlogn but no need of for loop
+		//we can directly access the mid element after sorting it will be the the dominant element
 		
-		System.out.println(arr[n/2]);
+//		System.out.println(arr[n/2]);
+		
+	/*
+	 * mo's algorithm*/
+		
+		for(int i = 1; i < n-1; i++) {
+			if(arr[i]==arr[i-1]) {
+				c++;
+				
+			} else {
+				
+				if(c==0) {
+					dominantElement = arr[i];
+					
+				} else {
+					c--;
+					
+				}
+			} 
+		}
+		System.out.println(dominantElement);
+		
+		
+		
 		
 	}
 }  //--total time complexity nlogn
